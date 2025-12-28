e12e06a67d1035e900be8710f82032ac8c740c4c
Euphorlc <euphoric.dock@gmail.com>
2025-12-28 14:57:02 +0800

    fix: automatic rulesets update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,20 @@ REPO_URL="https://github.com/euphorlc/corvoscan-rulesets.git"
 echo "[-] Initializing CorvoScan Environment..."
 
 # Handle Dynamic Rulesets
-if [ -d "$RULES_DIR" ]; then
+if [ -d "$RULES_DIR/.git" ]; then
     echo "[-] Updating existing rulesets..."
-    cd "$RULES_DIR"
+    cd "$RULES_DIR" || exit
     git pull origin main
 else
     echo "[-] Cloning rulesets for the first time..."
-    # Create dir if not exists
+    if [ -d "$RULES_DIR" ]; then
+        rm -rf "$RULES_DIR"/*
+    fi
     git clone "$REPO_URL" "$RULES_DIR"
 fi
 
 # Return to app dir
-cd /home/corvo/app
+cd /home/corvo/app || exit
 
 # Run the Application
 echo "[+] Starting CorvoScan GUI..."
